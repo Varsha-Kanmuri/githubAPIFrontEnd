dbbe006178bf0df347a87cc15c9d4aea5a7df099
Varsha Meghana <varshameghana30@gmail.com>
2019-02-26 07:01:12 +0530

    Service class which makes calls to backend server
    Service class which makes call to backend server , passing the owner name and reponame as aruments in the request

--- src/app/issues-service.service.ts
+++ src/app/issues-service.service.ts
@@ -11,6 +11,7 @@ export class IssuesServiceService {
 
   constructor(private http: HttpClient) { }
 
+  /*This function makes a call to the backend server , passing the owner name and reponame as aruments in the request*/
   fetchOpenIssuesCount(user: UserInfo): Observable<any> {
       
       return this.http.get(`http://127.0.0.1:5000/issues?owner=${user.owner}&reponame=${user.repositoryName}`);
